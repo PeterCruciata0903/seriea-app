2ad4302499515ed26664c98363a18ba02c22a6c2
Peter <pcruch@gmail.com>
2024-04-01 22:15:25 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import api from './api';
 
-function App() {
+const App = () => {
+  const [players, setPlayers] = useState([]);
+  const [formData, setFormData] = useState({
+    name: '',
+    age: '',
+    position: '',
+    nationality: '',
+    team: '',
+    value: ''
+  });
+
+  const fetchPlayers = async () => {
+    try {
+      const response = await api.get('/players/');
+      setPlayers(response.data);
+    } catch (error) {
+      console.error('Error fetching players:', error);
+      // Handle error, e.g., show a notification to the user
+    }
+  };
+  
+
+  useEffect(() => {
+    fetchPlayers();
+  }, []);
+
+  const handleInputChange = (event) => {
+    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+    setFormData({
+      ...formData,
+      [event.target.name]: value
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    await api.post('/players/', formData);
+    fetchPlayers();
+    setFormData({
+      name: '',
+      age: '',
+      position: '',
+      nationality: '',
+      team: '',
+      value: ''
+    })
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Serie A Transmarket Value</a>
+        </div>
+      </nav>
 
+      <table className='table table-striped table-bordered table-hover'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Position</th>
+            <th>Nationality</th>
+            <th>Team</th>
+            <th>Value</th> 
+          </tr>
+        </thead>
+        <tbody>
+          {players.map((player) => (
+            <tr key={player.id}>
+              <td>{player.name}</td>
+              <td>{player.age}</td>
+              <td>{player.position}</td>
+              <td>{player.nationality}</td>
+              <td>{player.team}</td>
+              <td>{player.value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      
+    </div>
+  )
+
+};
 export default App;
